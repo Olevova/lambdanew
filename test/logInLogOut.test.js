8b0bf9c9ba63cb5e296479ae1181f2e5b721e8d4
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2024-07-10 11:44:24 +0300

    testimo

--- test/logInLogOut.test.js
+++ test/logInLogOut.test.js
@@ -49,7 +49,7 @@ describe('Log In and Log Out Production, test-cases #1, 2', function() {
         await loginPageTest.checkSaveForFuture();
         await loginPageTest.login(config.urlHomePageForCheck);
         await logOutUserTest.findUserMenu();
-        await logOutUserTest.userLogOut(config.urlLoginPage);
+        await logOutUserTest.userLogOut(config.urlLoginPa);
         console.log('test passed');
       } catch (error) {
         makeScreenshot(driver, 'auth_test');
